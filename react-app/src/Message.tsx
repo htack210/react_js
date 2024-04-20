f866500bebe1f467e819fd049c47fb8d1b2c2d2c
htack210 <glenn.hline@outlook.com>
2024-04-20 12:36:32 -0400

    Created List Group using React and Bootstrap

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,12 @@
+// PascalCasing
+function Message(){
+    // JSX: JavaScript XML
+    // babeljs.io/repl
+    const name = 'Glenn'
+    if (name)
+        return <h1>Hello, {name}!</h1>;
+    return <h1>Hello, World!</h1>
+}
+
+// export default Message; Question;
+export default Message;
