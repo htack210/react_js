dbfbe84c0293cf038a1d1808a699284fec5eb6d0
htack210 <glenn.hline@outlook.com>
2024-04-20 19:09:06 -0400

    Added props and state

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,21 @@
-// import Message from "./Message";
 import ListGroup from "./components/ListGroup";
 
-function App(){
-  return <div><ListGroup/></div>;
+function App() {
+  let items = ["Boston", "Atlanta", "Miami", "Paris", "London"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
