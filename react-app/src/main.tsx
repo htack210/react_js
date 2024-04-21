d539162ae2909341e4bffd208f9e32f7f914fd69
htack210 <glenn.hline@outlook.com>
2024-04-21 18:02:00 -0400

    Updated main.tsx

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppList from "./AppListGroup.tsx";
-import AppAlert from "./AppAlert.tsx";
+// import AppAlert from "./AppAlert.tsx";
 import AppBtn from "./AppButton.tsx";
 import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppList />
-    <AppAlert />
+    {/* <AppAlert /> */}
     <AppBtn />
   </React.StrictMode>
 );
