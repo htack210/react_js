e8407f10910066671e4fea3eca53cebbdfb4949a
htack210 <glenn.hline@outlook.com>
2024-04-21 17:58:56 -0400

    Added dismissible alert

--- react-app/src/AppButton.tsx
+++ react-app/src/AppButton.tsx
@@ -1,12 +1,21 @@
 import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
-    <div>
-      <Button onClick={() => console.log("Clicked")}>
-        <i>My</i> Button
-      </Button>
-    </div>
+    <>
+      <div>
+        {alertVisible && (
+          <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+        )}
+        <Button color="danger" onClick={() => setAlertVisibility(true)}>
+          <i>My</i> Button
+        </Button>
+      </div>
+    </>
   );
 }
 export default App;
